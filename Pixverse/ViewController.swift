ecda0c4f5834a11f6b3bac0e3952cca79c871231
KazbekMusaev <145585208+KazbekMusaev@users.noreply.github.com>
2025-06-28 13:19:34 +0300

    Initial Commit

---
+++ Pixverse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pixverse
+//
+//  Created by KazbekMusaev on 28.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
